ee12bbd1d7aeff2ef2fab1e81d61f916164fba6f
AvyuktaNagrath <134120674+AvyuktaNagrath@users.noreply.github.com>
2024-08-30 02:48:49 -0400

    Backend Initialization
    downloaded packages and stuff

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(cors());
+
+console.log('MONGODB_URI:', MONGODB_URI);
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to MongoDB:', err);
+});
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
