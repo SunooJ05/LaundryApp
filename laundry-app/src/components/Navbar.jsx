056b74e9a57fb60ae08e96011d733b4932219935
Michael Chan <75458728+midcha@users.noreply.github.com>
2024-09-08 23:54:14 -0400

    Very preoperational navbar functionality
    Implemented basic animation, extensive and experimental CSS files, dropdown menu for links.

--- laundry-app/src/components/Navbar.jsx
+++ laundry-app/src/components/Navbar.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
+import {useState} from 'react';
 import styles from '../styles/nav.module.css';
 
-
 const Navbar = () => {
-  let isAdmin = false;
+  const [menuVisible, setMenuVisible] = useState(false);
+  const toggleMenu = () => {
+    setMenuVisible(prevState => !prevState)
+  };
+  
+
+  let isAdmin = true;
+  
   return (
     <nav className={styles.navbar}>
-      <h1>Ladro Padro</h1>
-      <div className={styles.links}>
-        <a href="/">Home</a>
-        {isAdmin && <a href="/adminview">Admin View</a>}
+      <div id={styles.top}>
+        <h1>GT Laundry</h1>
+        <button id={styles.dropdown} onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </button>
+      </div>
+      <div className={`${styles.links} ${menuVisible ? styles.show : styles.hide}`}>
         <a href="accountsetup">Account Setup</a>
+        {isAdmin && <a href="/adminview">Admin View</a>}
         <a href="/userview">User View</a>
+        <a href="/">Home</a>
       </div>
     </nav>
+    
   );
 };
+
 
 export default Navbar;
