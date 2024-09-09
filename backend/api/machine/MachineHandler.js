9310ff5cd164e70e645a252a18e173012b372028
Sunoo Jeong <sunoo.jeong@gmail.com>
2024-09-08 23:38:50 -0400

    API endpoint for Machines
    Authentication is to-do

---
+++ backend/api/machine/MachineHandler.js
@@ -0,0 +1,94 @@
+import Machine from '../../models/Machine.js';
+
+// Create a new machine
+// ADMIN AUTH TODO
+export const createMachine = async (req, res) => {
+    try {
+        const {type, status} = req.body;
+        const machine = new Machine({ type, status });
+        await machine.save();
+        res.status(201).json(machine);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Update machine position
+// ADMIN AUTH TODO
+export const updateMachinePosition = async (req, res) => {
+    const machineID = req.params.machineID;
+    const { locationX, locationY } = req.body;
+    try {
+        const machine = await Machine.findOneAndUpdate(
+            { machineID: machineID }, 
+            { locationX, locationY }, 
+            { new: true }
+        );
+        if (!machine) {
+            return res.status(404).json({ message: 'Machine not found' });
+        }
+        res.json(machine);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Delete a machine
+// ADMIN AUTH TODO
+export const deleteMachine = async (req, res) => {
+    try {
+        const machine = await Machine.findOneAndDelete({ machineID: req.params.machineID });
+        if (!machine) {
+            return res.status(404).json({ message: 'Machine not found' });
+        }
+        res.status(200).json({ message: 'Machine deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+// Get all machines
+export const getAllMachines = async (req, res) => {
+    try {
+        const machines = await Machine.find();
+        res.json(machines);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Get a specific machine by ID
+export const getMachineById = async (req, res) => {
+    try {
+        const machine = await Machine.findOne({ machineID: req.params.machineID });
+        if (!machine) {
+            return res.status(404).json({ message: 'Machine not found' });
+        }
+        res.json(machine);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+// Update a machine
+export const updateMachine = async (req, res) => {
+    try {
+        const { machineID, type, status, expectedEndTime, userID, locationX, locationY } = req.body;
+        const machine = await Machine.findOneAndUpdate(
+            { machineID: req.params.machineID },  // Search by machineID
+            { machineID, type, status, expectedEndTime, userID, locationX, locationY },
+            { new: true, runValidators: true }
+        );
+        if (!machine) {
+            return res.status(404).json({ message: 'Machine not found' });
+        }
+        res.json(machine);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+
--- backend/models/Machine.js
+++ backend/models/Machine.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
+import autoIncrement from '../util/autoIncrement.js';
 
 const machineSchema = new mongoose.Schema({
-    machineID: { type: Number, required: true, unique: true },
+    machineID: { type: Number, unique: true },
     type: { type: String, enum: ['washer', 'dryer'], required: true },
-    status: { type: String, enum: ['available', 'in-use', 'out-of-order'], required: true },
+    status: { type: String, enum: ['available', 'in-use', 'out-of-order'], required: true, default: 'available' },
+    locationX: { type: Number, require: true, default: 0},
+    locationY: { type: Number, require: true, default: 0},
     expectedEndTime: { type: Date, default: null }, // The time when the machine is expected to finish
     userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
 });
@@ -13,6 +16,13 @@ machineSchema.virtual('remainingTime').get(function () {
     const now = new Date();
     const diff = this.expectedEndTime - now; // difference in milliseconds
     return diff > 0 ? Math.floor(diff / 1000 / 60) : 0; // return remaining time in minutes
+});
+
+machineSchema.pre('save', async function (next) {
+    if (this.isNew) {
+        this.machineID = await autoIncrement('machineID');
+    }
+    next();
 });
 
 const Machine = mongoose.model('Machine', machineSchema);
